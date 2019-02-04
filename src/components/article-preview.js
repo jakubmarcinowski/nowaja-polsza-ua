d52321c9a27643590d4a4d62f5790ff823ed0883
zanetagorska <zaneta.gorska1@gmail.com>
2019-02-04 11:32:45 +0100

    Replace tabs with spaces

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -5,16 +5,16 @@ import Img from 'gatsby-image';
 import styles from './article-preview.module.css';
 
 export default ({ article }) => (
-	<div className={styles.preview}>
-		{/*<Img alt="" fluid={article.heroImage.fluid} />*/}
-		<h3 className={styles.previewTitle}>
-			<Link to={`/blog/${article.slug}`}>{article.title}</Link>
-		</h3>
-		<small>{article.publishDate}</small>
-		<div
-			dangerouslySetInnerHTML={{
-				__html: article.description.childMarkdownRemark.html
-			}}
-		/>
-	</div>
+  <div className={styles.preview}>
+    {/*<Img alt="" fluid={article.heroImage.fluid} />*/}
+    <h3 className={styles.previewTitle}>
+      <Link to={`/blog/${article.slug}`}>{article.title}</Link>
+    </h3>
+    <small>{article.publishDate}</small>
+    <div
+      dangerouslySetInnerHTML={{
+        __html: article.description.childMarkdownRemark.html
+      }}
+    />
+  </div>
 );
