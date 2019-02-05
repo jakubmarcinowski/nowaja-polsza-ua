e9f3262bda5ae881bf30b4e5e1d15898666cfd6d
michalnicinski <michalnicinski@gmail.com>
2019-02-05 13:25:07 +0100

    NPL-127: added contentful env var

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,8 @@ try {
 // Overwrite the Contentful config with environment variables if they exist
 contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken
+  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+  environment: process.env.CONTENTFUL_ENV // master|develop
 };
 
 const { spaceId, accessToken } = contentfulConfig;
