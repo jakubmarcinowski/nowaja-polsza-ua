44b64618bc45579f08b922295ed1bfde054cd84a
Jakub Maslon <jakubmaslon@gmail.com>
2019-02-06 14:17:32 +0100

    images build fails fixed

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,46 +1,47 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import Helmet from 'react-helmet';
-import get from 'lodash/get';
-import ImgValidator from '../components/ImgValidator';
-import Layout from '../components/layout';
+import React from 'react'
+import { graphql } from 'gatsby'
+import Helmet from 'react-helmet'
+import get from 'lodash/get'
+// import ImgValidator from '../components/ImgValidator'  // remove at all if not needed
+import Img from 'gatsby-image'
+import Layout from '../components/layout'
 
-import heroStyles from '../components/hero.module.css';
+import heroStyles from '../components/hero.module.css'
 
 class BlogPostTemplate extends React.Component {
   render() {
-    const post = get(this.props, 'data.contentfulBlogPost');
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title');
+    const post = get(this.props, 'data.contentfulBlogPost')
+    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
 
     return (
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
           <Helmet title={`${post.title} | ${siteTitle}`} />
           <div className={heroStyles.hero}>
-            <ImgValidator img={post.heroImage} />
+            <Img fluid={post.heroImage} />
           </div>
           <div className="wrapper">
             <h1 className="section-headline">{post.title}</h1>
             <p
               style={{
-                display: 'block'
+                display: 'block',
               }}
             >
               {post.publishDate}
             </p>
             <div
               dangerouslySetInnerHTML={{
-                __html: post.body.childMarkdownRemark.html
+                __html: post.body.childMarkdownRemark.html,
               }}
             />
           </div>
         </div>
       </Layout>
-    );
+    )
   }
 }
 
-export default BlogPostTemplate;
+export default BlogPostTemplate
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
@@ -49,7 +50,7 @@ export const pageQuery = graphql`
       publishDate(formatString: "MMMM Do, YYYY")
       heroImage {
         fluid(maxWidth: 1180, background: "rgb:000000") {
-          ...GatsbyContentfulFluid_tracedSVG
+          ...GatsbyContentfulFluid
         }
       }
       body {
@@ -59,4 +60,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
