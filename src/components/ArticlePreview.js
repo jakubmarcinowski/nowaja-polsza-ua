22409d88a2da786da3076729e0af14f30146d7bc
zanetagorska <zaneta.gorska1@gmail.com>
2019-02-08 09:18:50 +0100

    Replace old styles with styled components

---
+++ src/components/ArticlePreview.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import ImgValidator from './ImgValidator'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const StyledPreview = styled.div`
+  .previewTitle {
+    font-size: 1.5em;
+  }
+
+  .tag {
+    color: #a0a0a0;
+    text-decoration: none;
+    display: inline-block;
+    padding: 0.33333rem 0.5rem;
+    line-height: 1;
+    border-radius: 2px;
+    border: 1px solid #a0a0a0;
+    margin-right: 0.5em;
+  }
+`
+
+const ArticlePreview = ({ article }) => (
+  <StyledPreview>
+    <ImgValidator img={article.heroImage} />
+    <h3 className="previewTitle">
+      <Link to={`/blog/${article.slug}`}>{article.title}</Link>
+    </h3>
+    <small>{article.publishDate}</small>
+    <div
+      dangerouslySetInnerHTML={{
+        __html: article.description.childMarkdownRemark.html,
+      }}
+    />
+  </StyledPreview>
+)
+
+ArticlePreview.propTypes = {
+  article: PropTypes.shape({
+    description: PropTypes.shape({
+      childMarkdownRemark: PropTypes.shape({
+        html: PropTypes.string,
+      }),
+    }),
+    publishDate: PropTypes.string,
+    slug: PropTypes.string,
+    tags: PropTypes.arrayOf(PropTypes.string),
+    title: PropTypes.string,
+  }),
+}
+
+export default ArticlePreview
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,9 +2,20 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
-import styles from './blog.module.css'
-import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
+import Layout from '../components/Layout'
+import ArticlePreview from '../components/ArticlePreview'
+import styled from 'styled-components'
+
+const StyledHero = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 61.8vh;
+  max-height: 400px;
+  background: #e1e1e1;
+  font-size: 2em;
+  overflow: hidden;
+`
 
 class BlogIndex extends React.Component {
   render() {
@@ -15,7 +26,7 @@ class BlogIndex extends React.Component {
       <Layout>
         <div style={{ background: '#fff' }}>
           <Helmet title={siteTitle} />
-          <div className={styles.hero}>Blog</div>
+          <StyledHero>Blog</StyledHero>
           <div className="wrapper">
             <h2 className="section-headline">Recent articles</h2>
             <ul className="article-list">
