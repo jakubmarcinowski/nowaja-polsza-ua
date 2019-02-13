eddeeb3e8e27f052430cede3c69ed5105039ea83
michalnicinski <michalnicinski@gmail.com>
2019-02-13 16:26:42 +0100

    NPL-29: Added author's page

--- gatsby-node.js
+++ gatsby-node.js
@@ -39,5 +39,38 @@ exports.createPages = ({ graphql, actions }) => {
         })
       })
     )
+
+    const authorTemplate = path.resolve('./src/templates/author.js')
+    resolve(
+      graphql(
+        `
+          {
+            allContentfulPerson {
+              edges {
+                node {
+                  slug
+                }
+              }
+            }
+          }
+        `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        const authors = result.data.allContentfulPerson.edges
+        authors.forEach((author, index) => {
+          createPage({
+            path: `/author/${author.node.slug}/`,
+            component: authorTemplate,
+            context: {
+              slug: author.node.slug,
+            },
+          })
+        })
+      })
+    )
   })
 }
