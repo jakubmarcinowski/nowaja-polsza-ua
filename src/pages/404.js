6d634544d8a4a386f466e433a10d9aab01ee332f
michalnicinski <michalnicinski@gmail.com>
2019-02-14 10:12:21 +0100

    cr fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import Layout from '../components/Layout'
 import { Helmet } from 'react-helmet'
+
+import Layout from '../components/Layout'
 
 const Page404 = () => (
   <Layout>
-    <>
-      <Helmet title="Page not found" />
-      <div>404 Page not found</div>
-    </>
+    <Helmet title="Page not found" />
+    <div>404 Page not found</div>
   </Layout>
 )
 
