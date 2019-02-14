2d8213a7575728bb573a2609f53551d1e43ab8c0
michalnicinski <michalnicinski@gmail.com>
2019-02-14 10:10:26 +0100

    page not found

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Layout from '../components/Layout'
+import { Helmet } from 'react-helmet'
 
-const Page404 = () => {
-  return <div>404 Not found</div>
-}
+const Page404 = () => (
+  <Layout>
+    <>
+      <Helmet title="Page not found" />
+      <div>404 Page not found</div>
+    </>
+  </Layout>
+)
 
 export default Page404
