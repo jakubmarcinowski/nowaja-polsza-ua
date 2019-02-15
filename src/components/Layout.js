55d296769aab297810736bb4570e13a81a7d4570
zanetagorska <zaneta.gorska1@gmail.com>
2019-02-15 14:02:00 +0100

    Crete global style util and reset styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,41 +1,14 @@
 /* eslint-disable */
 import React from 'react'
-import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 
+import { GlobalStyle } from '../utils/global'
 import { theme } from '../utils/theme'
 import Container from './Container'
 import Navigation from './Navigation'
 import PageHeader from './PageHeader'
 import Footer from './Footer'
 import Rodo from './Rodo'
-
-const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: "Avenir";
-    font-weight: 400;
-    font-style: normal;
-    src: url("/avenir-400.woff2") format("woff2");
-    font-display: swap;
-  }
-
-  html {
-    box-sizing: border-box;
-    font-size: 62.5%;
-  }
-  *, *:before, *:after {
-    box-sizing: inherit;
-  }
-  body {
-    font-family: "Avenir", Tahoma, Arial, Helvetica, sans-serif;
-    padding: 0;
-    margin: 0;
-    font-size: 1.6rem;
-  }
-  a {
-    text-decoration: none;
-    color: currentColor;
-  }
-`
 
 class Layout extends React.Component {
   render() {
