fa6dadf75f0f5fd4e4a868532df14ddb09f598b2
zanetagorska <zaneta.gorska1@gmail.com>
2019-02-18 16:06:23 +0100

    Crete highlighted article for desktop

--- src/views/home/subcomponents/HighlightedArticle.js
+++ src/views/home/subcomponents/HighlightedArticle.js
@@ -3,32 +3,62 @@ import styled from 'styled-components'
 
 import ImgWrapper from '../../../components/ImgWrapper'
 import { articleType } from '../../../types/article'
+import { Link } from 'gatsby'
 
 const HighlightedArticleStyled = styled.div`
-  width: 80%;
-  padding: 24px;
-  border: 1px solid black;
-  margin: auto;
+  position: relative;
+  width: calc(100% * 7 / 12);
+  color: ${props => props.theme.colors.white};
+  font-weight: bold;
+
+  /* TODO replace by header later */
+  h2 {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+  }
+
+  h5 {
+    font-size: 1.6rem;
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(
+      to bottom,
+      rgba(17, 29, 34, 0.02),
+      rgba(17, 29, 34, 0.19) 39%,
+      rgba(17, 29, 34, 0.86)
+    );
+  }
+`
+const ArticleContent = styled.div`
+  position: absolute;
+  z-index: 1;
+  bottom: 10%;
+  left: 0;
+  right: 0;
+  transform: translate(0, -50%);
+  text-align: center;
 `
 
-const HighlightedArticle = ({
-  post: { title, slug, description, heroImage },
-}) => {
-  return (
-    <HighlightedArticleStyled>
-      <ImgWrapper img={heroImage} />
-      <h2>{title}</h2>
-      {description && (
-        <div
-          dangerouslySetInnerHTML={{
-            __html: description.childMarkdownRemark.html,
-          }}
-        />
-      )}
-      <button>{slug}</button>
-    </HighlightedArticleStyled>
-  )
-}
+const HighlightedArticle = ({ post: { title, slug, heroImage, author } }) => (
+  <HighlightedArticleStyled>
+    <ImgWrapper img={heroImage} />
+    <ArticleContent>
+      <h2>
+        <Link to={`/blog/${slug}`}>{title}</Link>
+      </h2>
+      <h5>
+        <Link to={`/author/${author.slug}`}>{author.name}</Link>
+      </h5>
+    </ArticleContent>
+  </HighlightedArticleStyled>
+)
 
 HighlightedArticle.propTypes = {
   post: articleType,
