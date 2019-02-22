a5141991d813b3c9c8e1e802b15f6622bdc6dc14
zanetagorska <zaneta.gorska1@gmail.com>
2019-02-22 12:35:28 +0100

    Overflow syntax console error fix

--- src/views/home/subcomponents/TheNewestItem.js
+++ src/views/home/subcomponents/TheNewestItem.js
@@ -50,7 +50,7 @@ const TheNewestItem = ({
           color="Dark"
           margin="0 0 1rem"
           lineHeight="Medium"
-          overflow
+          overflow="hidden"
           height="5.2"
         >
           <Link to={`/blog/${slug}`}>{title}</Link>
