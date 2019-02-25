890e57a988602bd6e740ce13d94cefa215c751dc
michalnicinski <michalnicinski@gmail.com>
2019-02-25 17:14:29 +0100

    article header fix

--- src/views/article/index.js
+++ src/views/article/index.js
@@ -4,11 +4,30 @@ import styled from 'styled-components'
 import { articleType } from '../../types/article'
 import PageHeader from './subcomponents/PageHeader'
 import { mediaQueries } from '../../utils/mediaQueries'
+import Header from '../../components/Header'
+import Author from '../../components/Author'
 
 const StyledArticle = styled.article`
   padding: 2rem 0;
   @media ${mediaQueries.tablet} {
     padding: 5rem 0;
+  }
+`
+
+const HeaderStyled = styled(Header)`
+  text-align: center;
+`
+
+const Authors = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: -2rem;
+  padding: 2rem;
+
+  @media ${mediaQueries.tablet} {
+    margin: -5rem;
+    padding: 5rem;
   }
 `
 
@@ -29,6 +48,16 @@ const ArticlePage = ({
           __html: body.childMarkdownRemark.html,
         }}
       />
+    )}
+    <HeaderStyled size="Biggest">об авторе</HeaderStyled>
+    {authors && authors.length > 1 ? (
+      <Authors>
+        {authors.map(element => (
+          <Author author={element} key={element.id} few />
+        ))}
+      </Authors>
+    ) : (
+      <Author author={authors[0]} />
     )}
   </StyledArticle>
 )
