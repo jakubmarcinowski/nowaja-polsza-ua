10645e53d5f81ca68c073785e3087372877c241a
zanetagorska <zaneta.gorska1@gmail.com>
2019-02-26 11:39:00 +0100

    Main page hero padding add

--- src/views/home/subcomponents/Hero.js
+++ src/views/home/subcomponents/Hero.js
@@ -5,6 +5,8 @@ import { childrenType } from '../../../types/children'
 import { mediaQueries } from '../../../utils/mediaQueries'
 
 const StyledHero = styled.div`
+  padding: 2.5rem 0;
+
   @media ${mediaQueries.large} {
     display: flex;
     align-items: stretch;
