63e7802ced1e3b8ebd5444c11f26f5c3f5bdc335
zanetagorska <zaneta.gorska1@gmail.com>
2019-02-27 12:03:27 +0100

    Create basic structure for about

--- src/components/StaticContent.js
+++ src/components/StaticContent.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-const StaticContent = ({ children }) => {
-  return <div>Static Content {children}</div>
+import Wrapper from './Wrapper'
+
+const StyledContent = styled.div``
+
+const StaticContent = ({ content }) => {
+  return (
+    <Wrapper>
+      <styledContent
+        dangerouslySetInnerHTML={{
+          __html: content.childMarkdownRemark.html,
+        }}
+      />
+    </Wrapper>
+  )
+}
+
+StaticContent.propTypes = {
+  content: PropTypes.shape({
+    content: PropTypes.shape({
+      childMarkdownRemark: PropTypes.shape({
+        html: PropTypes.string,
+      }),
+    }),
+  }),
 }
 
 export default StaticContent
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,45 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { graphql } from 'gatsby'
+import PropTypes from 'prop-types'
 
 import Layout from '../components/Layout'
 import StaticContent from '../components/StaticContent'
 
-const About = () => (
-  <Layout>
-    <Helmet title="About us" />
-    <StaticContent>test</StaticContent>
-  </Layout>
-)
+const About = ({ data }) => {
+  const {
+    title,
+    content,
+  } = data.allContentfulPrivacyPolicyStaticContent.edges[0].node
+
+  return (
+    <Layout>
+      <Helmet title={title} />
+      <StaticContent content={content} />
+    </Layout>
+  )
+}
+
+About.propTypes = {
+  data: PropTypes.any,
+}
 
 export default About
+
+export const aboutQuery = graphql`
+  query AboutQuery {
+    allContentfulPrivacyPolicyStaticContent {
+      edges {
+        node {
+          id
+          title
+          content {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
+      }
+    }
+  }
+`
