16eca3e36901ead1738e10db252bb40331c18226
zanetagorska <zaneta.gorska1@gmail.com>
2019-03-01 11:13:07 +0100

    Create publications page mockup

---
+++ src/pages/publications.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import { graphql } from 'gatsby'
+import PropTypes from 'prop-types'
+
+import Layout from '../components/Layout'
+import PublicationPage from '../views/publications'
+
+const Publications = ({ data }) => {
+  const publications = data.allContentfulPublication.edges
+
+  return (
+    <Layout>
+      <Helmet title="Publications" />
+      <PublicationPage publications={publications} />
+    </Layout>
+  )
+}
+
+Publications.propTypes = {
+  data: PropTypes.any,
+}
+
+export default Publications
+
+export const PublicationsPageQuery = graphql`
+  query PublicationQuery {
+    allContentfulPublication(sort: { fields: [publishDate], order: DESC }) {
+      edges {
+        node {
+          title
+          slug
+          publishDate(formatString: "DD MMMM YYYY", locale: "ru-RU")
+          authors {
+            id
+            name
+            slug
+          }
+          heroImage {
+            fluid(maxWidth: 768, resizingBehavior: SCALE) {
+              ...GatsbyContentfulFluid
+            }
+          }
+          lead
+          pdf {
+            file {
+              url
+            }
+          }
+          mobi {
+            file {
+              url
+            }
+          }
+          epub {
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
---
+++ src/views/publications/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Wrapper from '../../components/Wrapper'
+
+const PublicationPage = ({
+  title,
+  slug,
+  publicationDate,
+  authors,
+  heroImage,
+  lead,
+  pdf,
+  mobi,
+  epub,
+}) => (
+  <Wrapper>
+    <div>Test</div>
+  </Wrapper>
+)
+
+PublicationPage.propTypes = {}
+
+export default PublicationPage
