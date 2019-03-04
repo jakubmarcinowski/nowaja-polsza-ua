85e633305dc02dd2c4c0cb3e1b0af1b3d518d980
Jakub Maslon <jakubmaslon@gmail.com>
2019-03-04 14:14:35 +0100

    play icon when multimedia

--- src/components/ArticleItem.js
+++ src/components/ArticleItem.js
@@ -9,60 +9,97 @@ import Paragraph from './Paragraph'
 import ArticleInfoBox from './ArticleInfoBox'
 import Header from './Header'
 import PhotoLabel from '../components/PhotoLabel'
+import playIcon from '../../static/icon-play.svg'
 
 const ImgBox = styled.div`
   position: relative;
   margin-bottom: 1.5rem;
+
+  ${({ isMultimedia }) =>
+    isMultimedia &&
+    `&:after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: black;
+      opacity: 0.5;
+      pointer-events: none;
+    }`};
+`
+
+const IconPlay = styled.img`
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: 50%;
+  left: 50%;
+  width: 5rem;
+  height: 5rem;
+  z-index: 1;
+  cursor: pointer;
+
+  ${ImgBox}:hover & {
+    opacity: 0.8;
+  }
 `
 
 const ArticleItem = ({
   article: { title, slug, authors, categories, heroImage, publishDate, lead },
   noCategoryLabel,
-}) => (
-  <>
-    <ImgBox>
-      <Link to={`/blog/${slug}`}>
-        <ImgWrapper img={heroImage} aspectRatio={1.76} />
-      </Link>
-      {!noCategoryLabel && categories && (
-        <Link to={`/category/${categories[0].slug}`}>
-          <PhotoLabel color={categories[0].color}>
-            {categories[0].title}
-          </PhotoLabel>
-        </Link>
-      )}
-    </ImgBox>
-    <ArticleInfoBox
-      authors={authors}
-      publishDate={publishDate}
-    />
-    {slug && (
-      <>
-        {slug && (
-          <Header
-            weight="Bold"
-            type={2}
-            size="Big"
-            color="Dark"
-            margin="0 0 1.4rem"
-            lineHeight="Medium"
-            overflow="hidden"
-            height="6.2"
-          >
-            <Link to={`/blog/${slug}`}>{title}</Link>
-          </Header>
-        )}
-        {lead && (
+}) => {
+  const isMultimedia =
+    categories &&
+    categories.filter(({ slug }) => slug === 'multimedia').length > 0
+  return (
+    <>
+      <ImgBox isMultimedia={isMultimedia}>
+        {isMultimedia && (
           <Link to={`/blog/${slug}`}>
-            <Paragraph size="Big" lineHeight="Medium" weight="Light">
-              {lead}
-            </Paragraph>
+            <IconPlay src={playIcon} alt="Play icon" />
           </Link>
         )}
-      </>
-    )}
-  </>
-)
+        <Link to={`/blog/${slug}`}>
+          <ImgWrapper img={heroImage} aspectRatio={1.76} />
+        </Link>
+        {!noCategoryLabel && categories && (
+          <Link to={`/category/${categories[0].slug}`}>
+            <PhotoLabel color={categories[0].color}>
+              {categories[0].title}
+            </PhotoLabel>
+          </Link>
+        )}
+      </ImgBox>
+      <ArticleInfoBox authors={authors} publishDate={publishDate} />
+      {slug && (
+        <>
+          {slug && (
+            <Header
+              weight="Bold"
+              type={2}
+              size="Big"
+              color="Dark"
+              margin="0 0 1.4rem"
+              lineHeight="Medium"
+              overflow="hidden"
+              height="6.2"
+            >
+              <Link to={`/blog/${slug}`}>{title}</Link>
+            </Header>
+          )}
+          {lead && (
+            <Link to={`/blog/${slug}`}>
+              <Paragraph size="Big" lineHeight="Medium" weight="Light">
+                {lead}
+              </Paragraph>
+            </Link>
+          )}
+        </>
+      )}
+    </>
+  )
+}
 
 ArticleItem.propTypes = {
   article: articleType,
--- src/views/home/subcomponents/TheNewestItem.js
+++ src/views/home/subcomponents/TheNewestItem.js
@@ -9,6 +9,7 @@ import Header from '../../../components/Header'
 import { mediaQueries } from '../../../utils/mediaQueries'
 import ArticleInfoBox from '../../../components/ArticleInfoBox'
 import PhotoLabel from '../../../components/PhotoLabel'
+import playIcon from '../../../../static/icon-play.svg'
 
 const Wrapper = styled.div`
   display: flex;
@@ -17,6 +18,20 @@ const ImgBox = styled.div`
   position: relative;
   flex: 0 0 46%;
   margin-right: 2.5rem;
+
+  ${({ isMultimedia }) =>
+    isMultimedia &&
+    `&:after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: black;
+      opacity: 0.5;
+      pointer-events: none;
+    }`};
 `
 const ParagraphWrapper = styled.div`
   @media ${mediaQueries.desktop} {
@@ -25,54 +40,79 @@ const ParagraphWrapper = styled.div`
   }
 `
 
+const IconPlay = styled.img`
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: 50%;
+  left: 50%;
+  width: 5rem;
+  height: 5rem;
+  z-index: 1;
+  cursor: pointer;
+
+  ${ImgBox}:hover & {
+    opacity: 0.8;
+  }
+`
+
 const TheNewestItem = ({
   article: { title, slug, authors, categories, heroImage, publishDate, lead },
-}) => (
-  <Wrapper>
-    <ImgBox>
-      <Link to={`/blog/${slug}`}>
-        <ImgWrapper img={heroImage} aspectRatio={1} />
-      </Link>
-      {categories && (
-        <Link to={`/category/${categories[0].slug}`}>
-          <PhotoLabel color={categories[0].color}>
-            {categories[0].title}
-          </PhotoLabel>
-        </Link>
-      )}
-    </ImgBox>
-    <div>
-      {slug && (
-        <Header
-          weight="Bold"
-          type={2}
-          size="MediumBig"
-          color="Dark"
-          margin="0 0 1rem"
-          lineHeight="Medium"
-          overflow="hidden"
-          height="4.6"
-        >
-          <Link to={`/blog/${slug}`}>{title}</Link>
-        </Header>
-      )}
-      <ArticleInfoBox
-        authors={authors}
-        publishDate={publishDate}
-        size="Small"
-      />
-      {lead && (
-        <ParagraphWrapper>
+}) => {
+  const isMultimedia =
+    categories &&
+    categories.filter(({ slug }) => slug === 'multimedia').length > 0
+  return (
+    <Wrapper>
+      <ImgBox isMultimedia={isMultimedia}>
+        {isMultimedia && (
           <Link to={`/blog/${slug}`}>
-            <Paragraph size="Medium" lineHeight="Medium" weight="Light">
-              {lead}
-            </Paragraph>
+            <IconPlay src={playIcon} alt="Play icon" />
           </Link>
-        </ParagraphWrapper>
-      )}
-    </div>
-  </Wrapper>
-)
+        )}
+        <Link to={`/blog/${slug}`}>
+          <ImgWrapper img={heroImage} aspectRatio={1} />
+        </Link>
+        {categories && (
+          <Link to={`/category/${categories[0].slug}`}>
+            <PhotoLabel color={categories[0].color}>
+              {categories[0].title}
+            </PhotoLabel>
+          </Link>
+        )}
+      </ImgBox>
+      <div>
+        {slug && (
+          <Header
+            weight="Bold"
+            type={2}
+            size="MediumBig"
+            color="Dark"
+            margin="0 0 1rem"
+            lineHeight="Medium"
+            overflow="hidden"
+            height="4.6"
+          >
+            <Link to={`/blog/${slug}`}>{title}</Link>
+          </Header>
+        )}
+        <ArticleInfoBox
+          authors={authors}
+          publishDate={publishDate}
+          size="Small"
+        />
+        {lead && (
+          <ParagraphWrapper>
+            <Link to={`/blog/${slug}`}>
+              <Paragraph size="Medium" lineHeight="Medium" weight="Light">
+                {lead}
+              </Paragraph>
+            </Link>
+          </ParagraphWrapper>
+        )}
+      </div>
+    </Wrapper>
+  )
+}
 
 TheNewestItem.propTypes = {
   article: articleType,
